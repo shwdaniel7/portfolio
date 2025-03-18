6f4e86d6365da39c0d76d2c58c979ddb1c646d1e
dan <nihildaniel@gmail.com>
2025-03-17 22:10:32 -0400

    social media
    página de social media feita

---
+++ subpages/social media/script.js
@@ -0,0 +1,10 @@
+function toggleInfo(id) {
+    var element = document.getElementById(id);
+    
+    if (element.style.display === "none" || element.style.display === "") {
+      element.style.display = "block";
+    } else {
+      element.style.display = "none";
+    }
+  }
+  
